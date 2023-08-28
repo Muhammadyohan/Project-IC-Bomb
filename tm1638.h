9ad1b626709f8d326237036dfa2064e81613c596
Muhammadyohan <yohun2002@gmail.com>
2023-08-28 10:17:06 +0700

    Functions for TM1638 module.

---
+++ tm1638.h
@@ -0,0 +1,83 @@
+const int strobe = 7;
+const int clock = 9;
+const int data = 8;
+
+unsigned char TABLE7SEG[] = { 
+  0b00111111,
+  0b00000110,
+  0b01011011,
+  0b01001111,
+  0b01100110,
+  0b01101101,
+  0b01111101,
+  0b00000111,
+  0b01111111,
+  0b01101111};
+
+void sendCommand(uint8_t value)
+{
+  digitalWrite(strobe, LOW);
+  shiftOut(data, clock, LSBFIRST, value);
+  digitalWrite(strobe, HIGH);
+}
+
+void reset()
+{
+  sendCommand(0x40); // set auto increment mode
+  digitalWrite(strobe, LOW);
+  shiftOut(data, clock, LSBFIRST, 0xc0);   // set starting address to 0
+  for (uint8_t i = 0; i < 10; i++)
+  {
+    shiftOut(data, clock, LSBFIRST, 0x00);   // set 7seg0-7seg4 and led1-led5 to 0
+  }
+  shiftOut(data, clock, LSBFIRST, 0x40);   // set 7seg5 to 0
+  for (uint8_t j = 0; j < 5; j++)
+  {
+    shiftOut(data, clock, LSBFIRST, 0x00);   // set led6-led8 and 7seg6-7seg7 to 0
+  }
+  digitalWrite(strobe, HIGH);
+}
+
+void DISP_NUM_to_7SEG(char digit, uint8_t num)
+{
+  sendCommand(0x44); // set fix address
+  char num7seg;
+  char address = 0xc0;
+  digit = digit<<1;
+  address |= digit;
+  num7seg = TABLE7SEG[num];
+  digitalWrite(strobe, LOW);
+  shiftOut(data, clock, LSBFIRST, address);
+  shiftOut(data, clock, LSBFIRST, num7seg);
+  digitalWrite(strobe, HIGH);
+}
+
+void setTM1638LED(uint8_t value, uint8_t position)
+{
+  pinMode(data, OUTPUT);
+
+  sendCommand(0x44);
+  digitalWrite(strobe, LOW);
+  shiftOut(data, clock, LSBFIRST, 0xC1 + (position << 1));
+  shiftOut(data, clock, LSBFIRST, value);
+  digitalWrite(strobe, HIGH);
+}
+
+uint8_t readTM1638_Buttons(void)
+{
+  uint8_t buttons = 0;
+  digitalWrite(strobe, LOW);
+  shiftOut(data, clock, LSBFIRST, 0x42);
+
+  pinMode(data, INPUT);
+
+  for (uint8_t i = 0; i < 4; i++)
+  {
+    uint8_t v = shiftIn(data, clock, LSBFIRST) << i;
+    buttons |= v;
+  }
+
+  pinMode(data, OUTPUT);
+  digitalWrite(strobe, HIGH);
+  return buttons;
+}
